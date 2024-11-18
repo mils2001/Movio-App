924b738f74d224ae35b26dec916c7e5122696809
chris <Mileschris466@gmail.com>
2024-11-18 12:31:39 +0300

    routes

--- pages/Layout.jsx
+++ pages/Layout.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import Home from './'
+import React, from 'react';
+import React {Routes,Route,Link} from 'react-router';
+import Home from './Home'
+import About from './About'
+import Contact from './Contact'
+import { Route } from 'react-router-dom';
+
 
 function Layout() {
   return (
     <div>
-        
+        <Home/>
+        <About/>
+        <Contact/>
+
     </div>
   )
 }
